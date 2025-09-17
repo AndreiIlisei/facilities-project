4528802a5bbec758c2237eb521e7494011ef49f5
Andrei Ilisei <andrei.sv97@yahoo.com>
2025-09-17 13:07:01 +0200

    feat(stateManagement): add FacilitiesContext with reducer and localStorage persistence

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './styles/global.scss'
+import { FacilitiesProvider } from './state/FacilitiesContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FacilitiesProvider>
+      <App />
+    </FacilitiesProvider>
   </StrictMode>,
 )
